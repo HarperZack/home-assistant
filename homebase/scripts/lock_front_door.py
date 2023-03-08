94e2414b2f4aeac65c477157165a7cae4e7207f3
Zack <harper.zack@gmail.com>
2023-03-08 12:26:46 -0500

    God rid of unnecessary, dated code because of the setup file.

--- homebase/scripts/lock_front_door.py
+++ homebase/scripts/lock_front_door.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
-from pathlib import Path
-import sys
-sys.path.append(str(Path(__file__).parent.parent))
-# Run imports this way so importing works
 from homebase.appliances import lock
 
 if __name__ == '__main__':
     my_front_door = lock.get_first_lock()
+    my_front_door.lock()
