722202d988fd6c96f2f68878451400f1279f62c0
Zack <harper.zack@gmail.com>
2023-08-23 16:07:28 -0400

    Whoops, forgot a debug thing

--- homebase/main.py
+++ homebase/main.py
@@ -3,8 +3,6 @@ from homebase.appliances import plug
 import misc
 
 if __name__ == '__main__':
-    print(f'Current active access token is: {6}')
-
     my_front_door = lock.get_first_lock()
     interface = plug.Plug(misc.interface_id, misc.interface_ip, misc.interface_key)
     monitors = plug.Plug(misc.lp6_id, misc.lp6_ip, misc.lp6_key)
